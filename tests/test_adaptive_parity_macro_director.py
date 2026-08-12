8a02be7ebae2d087b55a4ea99fa451cddf1afafd
Julian Cooper <julianjcooper2002@gmail.com>
2026-08-12 16:18:07 +0200

    Revert "Pair first HQ with one safe mex upgrade"
    This reverts commit 9e3018ea9cd2dd73a9a037c9a2394bd75eaf402e.

--- tests/test_adaptive_parity_macro_director.py
+++ tests/test_adaptive_parity_macro_director.py
@@ -2667,8 +2667,7 @@ class TestRegionalMacro:
 
         assert mex_plan["hqAction"] == "start_t2"
         assert mex_plan["hqSourceToken"] == "land-a"
-        assert mex_plan["mexUpgradeSiteKeys"] == ["mex-0"]
-        assert mex_plan["mexUpgradeRolesBySite"]["mex-0"] == "mass_extractor_t2"
+        assert not mex_plan["mexUpgradeSiteKeys"]
         assert hq_plan["hqAction"] == "start_t2"
         assert not hq_plan["mexUpgradeSiteKeys"]
 
